15b8940efd182fc2a5464adc7e8830c543ad7263
PyGeek <80580345+willCodeForFood-exe@users.noreply.github.com>
2023-03-08 15:14:50 +0300

    Add project files.

---
+++ RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,13 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new("");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+
+
+Console.Read();
