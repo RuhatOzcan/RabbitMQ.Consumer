cd3502c510afe7b9f02d23872e51081f3a1ab23e
PyGeek <80580345+willCodeForFood-exe@users.noreply.github.com>
2023-03-08 15:15:23 +0300

    rabbitmq

--- RabbitMQ.Consumer/Program.cs
+++ RabbitMQ.Consumer/Program.cs
@@ -3,11 +3,21 @@ using RabbitMQ.Client.Events;
 using System.Text;
 
 ConnectionFactory factory = new();
-factory.Uri = new("");
+factory.Uri = new("amqps://vzraghwb:N15hjuQFbV18uoVcMsUmYKBWth0dgnwG@cow.rmq2.cloudamqp.com/vzraghwb");
 
 using IConnection connection = factory.CreateConnection();
 using IModel channel = connection.CreateModel();
 
+channel.QueueDeclare(queue: "example-queue", exclusive: false, durable: true);
 
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(queue: "example-queue", autoAck: false, consumer);
+channel.BasicQos(0, 1, false);
+
+consumer.Received += (sender, e) =>
+{
+    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+    channel.BasicAck(e.DeliveryTag, false);
+};
 
 Console.Read();
