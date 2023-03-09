08e497a729cd238fe6ae929c7a271f480daa5dc3
PyGeek <80580345+willCodeForFood-exe@users.noreply.github.com>
2023-03-09 13:47:59 +0300

    workerqueue

--- RabbitMQ.Consumer/Program.cs
+++ RabbitMQ.Consumer/Program.cs
@@ -8,16 +8,29 @@ factory.Uri = new("amqps://vzraghwb:N15hjuQFbV18uoVcMsUmYKBWth0dgnwG@cow.rmq2.cl
 using IConnection connection = factory.CreateConnection();
 using IModel channel = connection.CreateModel();
 
-channel.QueueDeclare(queue: "example-queue", exclusive: false, durable: true);
+string queueName = "example-work-queue";
+
+channel.QueueDeclare(
+    queue: queueName,
+    durable: false,
+    exclusive: false,
+    autoDelete: false);
 
 EventingBasicConsumer consumer = new(channel);
-channel.BasicConsume(queue: "example-queue", autoAck: false, consumer);
-channel.BasicQos(0, 1, false);
+
+channel.BasicConsume(
+    queue: queueName,
+    autoAck: true,
+    consumer: consumer);
+
+channel.BasicQos(
+    prefetchCount: 1,
+    prefetchSize: 0,
+    global: false);
 
 consumer.Received += (sender, e) =>
 {
     Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
-    channel.BasicAck(e.DeliveryTag, false);
 };
 
 Console.Read();
